e189fc581ce29ceea0e374342f7b048f7c8a045d
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-09-11 20:20:36 -0300

    Chamada do questionario transferida para o index

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-echo "<pre>";
-print_r($_GET);
+
+if ($_GET['url'] == 'questionario') {
+  require 'tela_questionario.php';
+
+  $t = new TelaQuestionario();
+
+  $perguntas[] = "Você é maior de idade?";
+  $perguntas[] = "Você come carne?";
+  $perguntas[] = "Você pratica esportes?";
+  $perguntas[] = "Você trabalha?";
+  $perguntas[] = "Você tem filhos";
+
+  echo $t->criaQuestionario($perguntas);
+} else {
+  echo "<pre>";
+  print_r($_GET);
+}
 
  ?>
