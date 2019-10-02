fec667e921777ce89108afd8fc75fb8a03dfe5d6
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-10-02 19:37:49 -0300

    Conexao com banco

--- dao/conexao.php
+++ dao/conexao.php
@@ -2,10 +2,24 @@
 class Conexao
 {
   private static $conexao;
+  private $servername = "localhost";
+  private $username = "adminer";
+  private $password = "ABC123**";
+  private $dbname = "escola";
+  private $conn;
 
   private function __construct()
   {
+    $this->conn = new PDO( "mysql:host=" . $this->servername . ";dbname=" .
+    $this->dbname,
+    $this->username,
+    $this->password );
+    $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
 
+  private function getConexao()
+  {
+    return $this->conn;
   }
 
   public function getInstance(){
@@ -14,6 +28,8 @@ class Conexao
     }
     return self::$conexao;
   }
+
+
 
 }
 
