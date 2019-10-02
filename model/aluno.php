1f452c7f7a8c48592559bfda938e93fa93deaed3
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-10-02 20:06:19 -0300

    Model Aluno

--- model/aluno.php
+++ model/aluno.php
@@ -1,11 +1,18 @@
 <?php
-
 class Aluno
 {
   private $id_aluno;
+  private $nome;
+  private $matricula;
+
+  public function getIdAluno(){return $this->id_aluno;}
+  public function setIdAluno($id_aluno){$this->id_aluno = $id_aluno;}
+
+  public function getNome(){return $this->nome;}
+  public function setNome($nome){$this->nome = $nome;}
+
+  public function getMatricula(){return $this->matricula;}
+  public function setMatricula($matricula){$this->matricula = $matricula;}
 
 }
-
-
-
- ?>
+?>
