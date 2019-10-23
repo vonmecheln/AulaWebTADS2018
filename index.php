65723b6167cfa02fbbef233f69dd85ae2de49250
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-10-23 15:44:34 -0300

    Novo tratamento de URL

--- index.php
+++ index.php
@@ -3,30 +3,35 @@
 include_once 'config.php';
 include_once 'autoload.php';
 
-$daoAluno = new DaoAluno();
-$lista_de_alunos = $daoAluno->getAll();
+//recupera a url digitada
+$u = isset($_GET['url']) ? trim($_GET['url']) : false;
 
-echo "<pre>";
-print_r($lista_de_alunos);
 
-die();
 
-if ($_GET['url'] == 'questionario') {
-
-  $t = new TelaQuestionario();
-  echo $t->formularioPesquisaPublico();
-
-} elseif ($_GET['url'] == 'respostas') {
-
-  $vr = new VerificaRespostas();
-  echo $vr->VerificaResposta();
-
-}
-
-else {
-  echo "<pre>";
-  echo "URL invalida:<br>";
-  print_r($_GET);
-}
+// $daoAluno = new DaoAluno();
+// $lista_de_alunos = $daoAluno->getAll();
+//
+// echo "<pre>";
+// print_r($lista_de_alunos);
+//
+// die();
+//
+// if ($_GET['url'] == 'questionario') {
+//
+//   $t = new TelaQuestionario();
+//   echo $t->formularioPesquisaPublico();
+//
+// } elseif ($_GET['url'] == 'respostas') {
+//
+//   $vr = new VerificaRespostas();
+//   echo $vr->VerificaResposta();
+//
+// }
+//
+// else {
+//   echo "<pre>";
+//   echo "URL invalida:<br>";
+//   print_r($_GET);
+// }
 
  ?>
