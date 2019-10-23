d2d62aec852320b6f4a9bf84070ec498f03c32cd
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-10-23 16:59:31 -0300

    Página Principal com Smarty

--- view/principal.php
+++ view/principal.php
@@ -5,7 +5,20 @@ class Principal
 
   function Home()
   {
-    echo "Página Principal";
+    $smarty = new Smarty();
+
+    $smarty->template_dir = 'view/templates/';
+    $smarty->compile_dir = 'view/templates_c/';
+    $smarty->cache_dir = 'view/cache/';
+    $smarty->caching = false;
+    $smarty->force_compile = true;
+
+
+    $smarty->assign('titulo', 'Plantas Prontas');
+    $smarty->assign('descricao', 'Projetos de plantas de casas prontas para construção');
+    $smarty->assign('p404', $_SESSION['p404']);
+
+    $smarty->display('home.tpl');
   }
 }
 
---
+++ view/templates/home.tpl
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>{$titulo}</title>
+  <meta charset="UTF-8">
+  <meta name="description" content="{$descricao}" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <div class="container">
+    {if $p404}
+    <div>
+      <strong>Atenção!</strong> Pagina não encontrada: {$p404}
+    </div>
+    {/if}
+
+
+    <h1>Você está na pagina principal!!</h1><br>
+    <h2>Conheça o nossos projetos</h2>
+    
+  </div>
+</body>
+</html>
